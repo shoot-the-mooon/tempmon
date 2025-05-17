fb33bcf7def42015a4ff5bc33444cba2f9451ae5
shoot-the-mooon <kensuke.s.com@gmail.com>
2025-05-18 00:49:58 +0900

    allowedhostにip追加

--- tempmon/settings.py
+++ tempmon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-dnr)+&oasc+p911_j#0p7uf#0vtzvj(e1##5%0#^olc-t3_m^x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.114.230.157']
 
 
 # Application definition
